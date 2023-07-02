41fe259f2fa9199379813f0dea1220df2f149c9e
Prahlad Pallav <135969574+prahlad-pallav@users.noreply.github.com>
2023-07-02 06:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let searchButton = document.querySelector('.search-btn');
 let searchText = document.querySelector('.news-input');
 
-const Api_Key = 'f9af11fe5d8d4c589a2ff9709e3f685f';
+const API_Key ='390f8ae808e87049241bcacefe8844d0';
 
-const url = 'https://newsapi.org/v2/everything?q=';
+const url ='https://gnews.io/api/v4/search?q=';
 
 window.addEventListener('load', () => News('politics'));
 function reload(){
     window.location.reload(); 
 }
 
+
 async function News(query){
-    let response = await fetch(`${url}${query}&apiKey=${Api_Key}`);
+    let response = await fetch(`${url}${query}&apikey=${API_Key}`);
     let data = await response.json();
     bindData(data.articles);
 }
@@ -23,7 +24,7 @@ function bindData(articles){
     cardContainer.innerHTML='';
 
     articles.forEach(article => {
-        if(!article.urlToImage) return;
+        if(!article.image) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
         dataFilling(cardClone, article);
         cardContainer.appendChild(cardClone);
@@ -36,7 +37,7 @@ function dataFilling(cardClone, article){
     let newsSource = cardClone.querySelector('#news-source');
     let newsDesc = cardClone.querySelector('#news-desc');
 
-    newsImg.src = article.urlToImage;
+    newsImg.src = article.image;
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
 
